110a345a3537b169dca8b6dcd103bdf87830c50e
Conor Owens <conorj.owens@gmail.com>
2019-07-28 17:24:58 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! NOBODY LOVES YOU!"
+    "Welcome to your app!!!! EVERYBODY LOVES YOU!"
   end
 
 end
